ab8bf4a73243e502497a0db41c869cc55e08043f
t-nakamura <im9today@me.com>
2022-04-06 01:35:15 +0900

    add .nojekyll, change output folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pt/",
+  base: "./",
+  build: {
+    assetsDir: "./",
+    outDir: "docs",
+  },
 });
